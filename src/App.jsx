bd75959ba3afcc666dba7cc61987f1012d71cac7
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-08-08 23:28:50 +0530

    created auth page alongwith sign in form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/signin/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
   return (
     <>
-      <h1>I am the shop pageshop</h1>
-      <h1>I am the shop pageshop</h1>
       <h1>I am the shop pageshop</h1>
     </>
   );
@@ -18,7 +16,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
