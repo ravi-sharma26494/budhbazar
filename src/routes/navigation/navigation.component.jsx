105bff30a360ab2ff3b1c6468660a92d7a01103e
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-08-12 00:38:16 +0530

    Cart Dropdown created

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,15 @@
 import React, { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import "./navigation.styles.scss";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from "../../contexts/user.context";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import "./navigation.styles.scss";
+
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   // console.log(currentUser);
@@ -27,7 +33,9 @@ const Navigation = () => {
               SIGNIN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </>
