2bee255e70009992b1174afedf6d1c28be41b2e9
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-08-24 23:56:34 +0530

    CategoriesContext(X)->redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import { CartProvider } from "./contexts/cart.context.jsx";
 import { CategoriesProvider } from "./contexts/categories.context.jsx";
-import { Provider } from "react-redux";
-import "./index.scss";
 import { store } from "./store/store.js";
+
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* <UserProvider> */}
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-        {/* </UserProvider> */}
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,12 +1,13 @@
-import React, { Fragment, useContext, useEffect, useState } from "react";
-import "./category.styles.scss";
+import React, { Fragment, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { CategoriesContext } from "../../contexts/categories.context";
+import "./category.styles.scss";
 import ProductCard from "../../components/product-card/product-card.component";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
