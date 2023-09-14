77e9f8426b44b34b9fa3938194b566d65916d371
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-09-14 23:56:40 +0530

    redux-saga/replace categoriesAsynThunkWid categorySaga/154

--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,21 @@ import { rootReducer } from "./root-reducer";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
 };
+
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -29,5 +33,6 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
